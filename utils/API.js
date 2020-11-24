1d328fe96773cf1284a9a492cafa81b41de28f8e
mslee001 <megtej@gmail.com>
2020-11-23 20:31:12 -0800

    Updates

--- utils/API.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable import/no-anonymous-default-export */
-import axios from "axios";
-
-export default {
-  // Gets all users
-  getUsers: function() {
-    return axios.get("https://randomuser.me/api/?results=200&nat=us");
-  }
-};
