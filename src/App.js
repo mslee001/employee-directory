5a84ce4bb16e28eb5fed463cd0c628f7070fdd37
mslee001 <megtej@gmail.com>
2020-11-23 20:44:50 -0800

    Revert "Updates"
    This reverts commit 1d328fe96773cf1284a9a492cafa81b41de28f8e.
    
    	new file:   .gitignore
    	new file:   README.md
    	deleted:    asset-manifest.json
    	deleted:    index.html
    	new file:   package-lock.json
    	new file:   package.json
    	renamed:    favicon.ico -> public/favicon.ico
    	new file:   public/index.html
    	renamed:    logo192.png -> public/logo192.png
    	renamed:    logo512.png -> public/logo512.png
    	renamed:    manifest.json -> public/manifest.json
    	renamed:    robots.txt -> public/robots.txt
    	new file:   src/App.css
    	new file:   src/App.js
    	new file:   src/App.test.js
    	new file:   src/components/EmployeeResultContainer.js
    	new file:   src/components/Header.js
    	new file:   src/components/ResultList.js
    	new file:   src/index.css
    	new file:   src/index.js
    	new file:   src/logo.svg
    	new file:   src/reportWebVitals.js
    	new file:   src/setupTests.js
    	new file:   src/utils/API.js
    	deleted:    static/css/2.1a02f21c.chunk.css
    	deleted:    static/css/2.1a02f21c.chunk.css.map
    	deleted:    static/css/main.7d180cbf.chunk.css
    	deleted:    static/css/main.7d180cbf.chunk.css.map
    	deleted:    static/js/2.a774bfd1.chunk.js
    	deleted:    static/js/2.a774bfd1.chunk.js.LICENSE.txt
    	deleted:    static/js/2.a774bfd1.chunk.js.map
    	deleted:    static/js/3.d973f080.chunk.js
    	deleted:    static/js/3.d973f080.chunk.js.map
    	deleted:    static/js/main.06d10823.chunk.js
    	deleted:    static/js/main.06d10823.chunk.js.map
    	deleted:    static/js/runtime-main.07d76f15.js
    	deleted:    static/js/runtime-main.07d76f15.js.map
    	new file:   utils/API.js

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './App.css';
+import EmployeeResultContainer from './components/EmployeeResultContainer';
+
+function App() {
+  return (
+    <EmployeeResultContainer />
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Header () {
+    return (
+        <div>
+            <div className="jumbotron">
+                <h1 className="display-3">Employee Directory</h1>
+                <p className="lead"> Click on "Name" to sort or use the search box to filter results</p>
+            </div>
+        </div>
+ )
+}
+
+export default Header;
+
+
+
